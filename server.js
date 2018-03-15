a7641c4542d36200bf547b8f3c0cc8a9e73bb64e
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-15 23:00:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,21 +90,17 @@ return htmlTemplate;
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/',function(req,res){
+    res.sendFile(path,join(__dirname,'ui','index.html'));
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName==aticle-one
 //articles[articleName]=={}content object for article one
+ var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-    
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-   });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
