e1a22357329d243276206165ece17a23b6fbc8d0
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-15 22:27:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,11 +2,60 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articleOne={
+    title:'Article-one@Alisha',
+    heading:'Welcome',
+    heading1:'Take Care....',
+content:`<p>Hellooo guysss....Hope all are fine...Here is my first article and as i said this is the content of the page 
+       and now you can see it....
+       enjoyyyy reading it....
+    </p>
+     <p>Hellooo guysss....Hope all are fine...Here is my first article and as i said this is the content of the page 
+       and now you can see it....
+       enjoyyyy reading it....
+    </p>
+     <p>Hellooo guysss....Hope all are fine...Here is my first article and as i said this is the content of the page 
+       and now you can see it....
+       enjoyyyy reading it....
+    </p>`
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var heading1=data.heading1;
+var htmlTemplate=`
+<html>
+    <head>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <title>${title}</title>
+        <div id="id1">
+    <div><a href="/">Home</a></div>
+    <br>
+    <br>
+    <div>
+        <h2>${heading}</h2>
+    </div>
+    <br>
+    <p>
+    ${content}
+    </p>
+    <h3>
+       ${heading1}
+    </h3>
+    </div>
+    </body>
+</html>`
+;
+return htmlTemplate;
+}
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
