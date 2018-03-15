80d83d884bde5e9c0b72aece32c8a842a4823f19
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-16 01:17:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/',function(req,res){
-    res.sendFile(path,join(__dirname,'ui','index.html'));
+    res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
 app.get('/:articleName', function (req, res) {
