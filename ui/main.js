0fc899263c7a35843215f27328edcbf0c0c68d6d
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-16 22:58:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var a=document.getElementById('i1');
 a.innerHTML='happyy days..';
+
+//moving element
+var b=document.getElementById('maddy');
+b.onclick=function(){
+  b.style.marginLeft='100px';  
+};
