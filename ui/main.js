258b90e8d7db3802a42ce35084f00262c0723e1c
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-16 23:09:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,14 @@ a.innerHTML='happyy days..';
 
 //moving element
 var b=document.getElementById('maddy');
+var marginLeft=0;
+function f1()
+{
+    marginLeft=marginLeft+10;
+    b.style.marginLeft=marginLeft+'px'
+}
+
 b.onclick=function(){
-  b.style.marginLeft='100px';  
+  //b.style.marginLeft='100px';
+  var v=setInterval(f1,100);
 };
