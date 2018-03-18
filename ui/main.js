ed012053bb0b9e6d017b8405f261193b0e164cd4
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-18 13:04:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,14 +34,31 @@ b1.onclick=function(){
 var ipname=document.getElementById('id1');
 var id1=ipname.value;
 var sub=document.getElementById('id4');
-sub.onclick=function(){
+
+sub.onclick=function(){  
+   //making request object
+  var request=new XMLHttpRequest();
   
-  var names=['name1','name2','name3','name4'];
-  var list='';
-  for(var i=0;i<names.length;i++)
-  {
-      list+='<li>' +names[i]+'<li>';
-  }
-    var ul=document.getElementById('ul1');
-    ul1.innerHTML=list;
+  
+  //response check
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+          if(request.status===200)
+          {
+             var names=request.responseText;
+             names=JSON.parse(names);
+             var list='';
+            for(var i=0;i<names.length;i++)
+            {
+                list+='<li>' +names[i]+'<li>';
+            }
+            var ul=document.getElementById('ul1');
+            ul1.innerHTML=list;             
+          }
+      }
+};
+ //making request
+    request.open('GET','http://alishakareem13.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
 };
