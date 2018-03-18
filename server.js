83048d8f5cc753809103cc1eb5fa5f252cdeb631
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-18 12:42:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-var names[];
+var names=[];
 app.get('/submit-name/:name',function(req,res){
    var name=req.params.name;
    names.push(name);
