90196eef3328bb61847847b81e96f86182793dc7
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-22 22:26:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return responses
-    pool.query('SELECT * from test',function(err,result){
+    pool.query('SELECT * FROM test',function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } 
