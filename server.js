0afb300098dbf83f53467511c9bf9a042d0c1581
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-22 23:00:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName==aticle-one
 //articles[articleName]=={}content object for article one
 // var articleName=req.params.articleName;
-pool.query("SELECT * FROM article WHERE title" + req.params.articleName,function(err,result){
+pool.query("SELECT * FROM article WHERE title='" + req.params.articleName +"'",function(err,result){
     if(err)
     {
         res.send(500).send(err.toString());
