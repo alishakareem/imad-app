6f4cbffa9bffcba9423adfcc2e3919d14ac48609
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-22 22:23:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').Pool;
+
+var config={
+    user:'alishakareem13',
+    database:'alishakareem13',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var articles={
     'article-one':{
@@ -90,11 +99,24 @@ return htmlTemplate;
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/test-db',function(req,res){
-    
-});
+
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
+});
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return responses
+    pool.query('SELECT * from test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } 
+       else
+       {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 var names=[];
