b56713ebac8fa79452537fca7848b3feb929b75d
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-22 23:01:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,13 +140,13 @@ app.get('/articles/:articleName', function (req, res) {
 pool.query("SELECT * FROM article WHERE title='" + req.params.articleName +"'",function(err,result){
     if(err)
     {
-        res.send(500).send(err.toString());
+        res.status(500).send(err.toString());
     }
     else
     {
         if(result.rows.length===0)
         {
-            res.send(404).send('article not found');
+            res.status(404).send('article not found');
         }
         else
         {
