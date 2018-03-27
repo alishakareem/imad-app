b747381be5a3c33485a4be3150df0fa4b226145a
alishakareem <33855600+alishakareem@users.noreply.github.com>
2018-03-27 23:54:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,21 +63,16 @@ app.post('/create-user',function(req,res){
    var password=req.body.password;
    var salt=crypto.randomBytes(128).toString('hex');
    var dbString=hash(password,salt);
-   pool.query('INSERT into "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+   pool.query('INSERT into "user" (username,password) VALUES ($1,$2)', [username,dbString] ,function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } 
        else
        {
-           res.send('user created successfully' +username);
+           res.send('user created successfully:' + username);
        }
    });
 });
-
-
-
-
-
 
 
 function hash(input,salt)
